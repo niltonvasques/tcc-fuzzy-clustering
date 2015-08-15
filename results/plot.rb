fa613e8d2c196bc3c1d3c77ccd37589f7e74bcf8
Nilton Vasques <nilton.vasques@gmail.com>
2015-08-15 15:57:50 -0300

    Improves plot graphics script

--- results/plot.rb
+++ results/plot.rb
@@ -62,6 +62,23 @@ File.open("#{base}.dat", 'w') do |file|
     file.write "\n"
   end
 end
-system( "./gplot.sh #{base}")
+#system( "./gplot.sh #{base}")
+%x( 
+gnuplot <<- EOF
+set title "#{base} base"
+FCM = "#99ffff"; PCM = "#0072d5"; HEFCM = "#4672d5"; HEFSFCM = "#ff71d5";
+set auto x
+set yrange [0:100]
+set style data histogram
+set style histogram cluster gap 1
+set style fill solid border -1
+set boxwidth 0.9
+set xtic scale 0
+# 2, 3, 4, 5 are the indexes of the columns; 'fc' stands for 'fillcolor'
+set term png
+set output "#{base}.png"
+plot '#{base}.dat' using 2:xtic(1) ti col fc rgb FCM, '' u 3 ti col fc rgb PCM, '' u 4 ti col fc rgb HEFCM, '' u 5 ti col fc rgb HEFSFCM
+EOF
+)
 system( "eog #{base}.png" )
 
